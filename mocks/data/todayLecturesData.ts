27def5650dd68386aeb54a995ce589def2a5fd05
moonyerim2 <moonyerim2@gmail.com>
2023-03-15 03:31:49 +0900

    feat: 오늘의 강의 목데이터 추가

---
+++ mocks/data/todayLecturesData.ts
@@ -0,0 +1,23 @@
+export const todayLecturesData = [
+  {
+    lecture: {
+      title: '심볼(Symbol)',
+    },
+    progress: 0,
+    completed: false,
+  },
+  {
+    lecture: {
+      title: '숫자, 수학 method (Number, Math)',
+    },
+    progress: 0,
+    completed: false,
+  },
+  {
+    lecture: {
+      title: '문자열 메소드(String methods)',
+    },
+    progress: 0,
+    completed: false,
+  },
+];
