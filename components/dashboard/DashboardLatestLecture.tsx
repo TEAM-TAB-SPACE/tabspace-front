7f9b15bda7321dca3b0a9edb8ca5bafdb57afd90
moonyerim2 <moonyerim2@gmail.com>
2023-03-14 19:42:52 +0900

    feat: 가장 최근 강의 hover 인터랙션 적용

--- components/dashboard/DashboardLatestLecture.tsx
+++ components/dashboard/DashboardLatestLecture.tsx
@@ -1,10 +1,20 @@
 import Link from 'next/link';
+import variables from '../../styles/variables.module.scss';
+import layout from '../../styles/layout.module.scss';
 
 function DashboardLatestLecture() {
   return (
     <>
       <div className="dashboard__latestLecture">
-        <Link href="/lecture" className="latestLecture__img"></Link>
+        <div className="latestLecture__wrapper">
+          <Link
+            href="/lecture"
+            className={`latestLecture__link ${layout.flex_center}`}
+          >
+            이어보기
+          </Link>
+          <div className="latestLecture__img"></div>
+        </div>
       </div>
       <style jsx global>{`
         .dashboard__latestLecture {
@@ -15,16 +25,49 @@ function DashboardLatestLecture() {
           overflow: hidden;
         }
 
-        .latestLecture__img {
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          background: url('http://img.youtube.com/vi/G360D6lqrfo/0.jpg')
-            no-repeat;
-          background-size: cover;
-          background-position: center center;
+        .latestLecture {
+          &__wrapper {
+            display: block;
+            width: 100%;
+            height: 100%;
+          }
+
+          &__img {
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 0;
+            width: 100%;
+            height: 100%;
+            color: ${variables.white};
+            background: url('http://img.youtube.com/vi/G360D6lqrfo/0.jpg')
+              no-repeat;
+            background-size: cover;
+            background-position: center center;
+            filter: brightness(70%);
+          }
+
+          &__link {
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 2;
+            width: 100%;
+            height: 100%;
+            font-size: 15px;
+            font-weight: 700;
+            color: ${variables.white};
+
+            &:hover {
+              color: ${variables.white};
+            }
+          }
+
+          &__link:hover + &__img {
+            transform: scale(1.1);
+            transition: transform 0.5s, filter 0.5s;
+            filter: brightness(45%);
+          }
         }
       `}</style>
     </>
