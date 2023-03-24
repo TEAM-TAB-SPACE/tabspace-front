f829f227d8942e0b4249b54750d99dc3b90f3641
moonyerim2 <moonyerim2@gmail.com>
2023-03-25 01:34:46 +0900

    feat: 목 서버 authHandler 추가

--- mocks/handler/handler.ts
+++ mocks/handler/handler.ts
@@ -1,9 +1,11 @@
 import { otherHandler } from './other';
 import { lectureHandler } from './lecture/lecture';
-import { dashboardHandler } from './dashboard.ts/dashboard';
+import { dashboardHandler } from './dashboard/dashboard';
+import { authHandler } from './auth';
 
 export const handlers = [
   ...lectureHandler,
   ...dashboardHandler,
   ...otherHandler,
+  ...authHandler,
 ];
