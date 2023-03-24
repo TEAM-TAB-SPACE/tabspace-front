fd721593bdd74564f0c2e1672b74094f8240b711
moonyerim2 <moonyerim2@gmail.com>
2023-03-25 00:34:22 +0900

    feat: 사이트 접속 시 로그인 유지를 위해 accessToken 재발급 로직 BaseLayout으로 이동 - 로그인 상태 전역 상태로 저장

--- components/layout/BaseLayout.tsx
+++ components/layout/BaseLayout.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { getCookie, setCookie } from 'cookies-next';
 import Header from './Header';
 import Footer from './Footer';
+import useFetch from '../../hooks/useFetch';
+import { loginStateAtom } from '../../store/user';
+import { API_URL_AUTH } from '../../pages/api/auth';
 
 type BaseLayoutProps = {
   children: React.ReactNode;
 };
 
 export default function BaseLayout({ children }: BaseLayoutProps) {
+  const fetch = useFetch();
+  const setIsLogin = useSetRecoilState(loginStateAtom);
+
+  useEffect(() => {
+    //사이트 접속 시 새로운 토큰 발행
+    const getNewAccessToken = async () => {
+      const refreshToken = getCookie('refreshToken');
+
+      if (refreshToken) {
+        const data = await fetch.post(API_URL_AUTH.REFRESH, {
+          refresh: refreshToken,
+        });
+        setCookie('accessToken', data.access);
+        setIsLogin(true);
+      }
+    };
+
+    getNewAccessToken();
+  }, [fetch, setIsLogin]);
+
   return (
     <>
       <Header />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,13 @@
 import '../styles/globals.css';
 import { Reset } from 'styled-reset';
 import { RecoilRoot } from 'recoil';
-import { useEffect } from 'react';
+import { ConfigProvider } from 'antd';
+import { isDevMode } from '../config/config.export';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import Script from 'next/script';
 import BaseLayout from '../components/layout/BaseLayout';
 import locale from 'antd/lib/locale/ko_KR';
-import useFetch from '../hooks/useFetch';
-import { ConfigProvider } from 'antd';
-import { getCookie, setCookie } from 'cookies-next';
-import { isDevMode } from '../config/config.export';
-import { API_URL_AUTH } from './api/auth';
 
 if (isDevMode && process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
   import('../mocks');
@@ -25,29 +21,11 @@ declare global {
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const fetch = useFetch();
-
   function kakaoInit() {
     // 페이지가 로드되면 실행
     window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_KEY);
     console.log(window.Kakao.isInitialized());
   }
-
-  useEffect(() => {
-    //사이트 접속 시 새로운 토큰 발행
-    const getNewAccessToken = async () => {
-      const refreshToken = getCookie('refreshToken');
-
-      if (refreshToken) {
-        const data = await fetch.post(API_URL_AUTH.REFRESH, {
-          refresh: refreshToken,
-        });
-        setCookie('accessToken', data.access);
-      }
-    };
-
-    getNewAccessToken();
-  }, [fetch]);
 
   return (
     <>
