2be96ba13f24df8d0225ccb066833197cbaa38f2
moonyerim2 <moonyerim2@gmail.com>
2023-03-26 23:52:15 +0900

    fix: 모달창 닫기 버튼 동작 안하는 오류 수정

--- components/userBadge/modal/DeleteModal.tsx
+++ components/userBadge/modal/DeleteModal.tsx
@@ -1,16 +1,23 @@
 import { Button, Modal } from 'antd';
+import { MouseEventHandler } from 'react';
 
 interface DeleteModalProps {
   isModalOpen: boolean;
+  onCancel: MouseEventHandler;
   onClickDelete?: () => void;
 }
 
 const DELETE_CONFIRM_MESSAGE = '댓글을 삭제하시겠습니까?';
 
-function DeleteModal({ isModalOpen, onClickDelete }: DeleteModalProps) {
+function DeleteModal({
+  isModalOpen,
+  onCancel,
+  onClickDelete,
+}: DeleteModalProps) {
   return (
     <Modal
       open={isModalOpen}
+      onCancel={onCancel}
       style={{ top: '40%' }}
       width="280px"
       footer={[
