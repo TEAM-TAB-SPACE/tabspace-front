5ea3acc46169f7811e136ec66c524b926a4d8f1b
moonyerim2 <moonyerim2@gmail.com>
2023-04-19 00:13:17 +0900

    refactor: Header 컴포넌트에서 logout 기능 훅으로 분리 - useAuth 훅 내부로 이동

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -1,36 +1,14 @@
 import { useRouter } from 'next/router';
-import { axiosInstance, setAxiosInterCeptors } from '../../../pages/api/axios';
-import { getCookie } from 'cookies-next';
 import Link from 'next/link';
 import css from 'styled-jsx/css';
 import Logo from '../../../public/assets/mainLogo.svg';
 import LoginContent from './LoginContent';
 import useAuth from '../../../hooks/useAuth';
-import { API_URL_AUTH } from '../../../pages/api/auth';
+import variables from '../../../styles/variables.module.scss';
 
 export default function Header() {
   const router = useRouter();
-  const { isLogin, user, setLogoutState } = useAuth();
-
-  setAxiosInterCeptors(axiosInstance);
-
-  // 로그아웃
-  const Logout = async () => {
-    try {
-      const accessToken = getCookie('access');
-
-      axiosInstance.post(API_URL_AUTH.LOGOUT, null, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-
-      setLogoutState();
-      location.href = '/';
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const { isLogin, user, logout } = useAuth();
 
   return (
     <>
@@ -50,7 +28,7 @@ export default function Header() {
         ) : (
           <LoginContent
             user={user}
-            onClickLoginout={Logout}
+            onClickLoginout={logout}
             onClickDashboard={() => {
               router.push('/dashboard');
             }}
@@ -64,16 +42,16 @@ export default function Header() {
 
 const header = css`
   .header__container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
     position: fixed;
     top: 0;
     left: 0;
     z-index: 1000;
     width: 100%;
-    background-color: #fff;
-    display: flex;
-    justify-content: space-between;
+    background-color: ${variables.white};
     padding: 15px 70px;
-    align-items: center;
     border-bottom: 1px solid #e2e8f0;
     box-shadow: 0px 2px 20px rgba(0, 0, 0, 0.15);
   }
--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -2,23 +2,25 @@ import { useCallback } from 'react';
 import { deleteCookie, setCookie } from 'cookies-next';
 import { isDevMode } from '../config/config.export';
 import { useRecoilState } from 'recoil';
-import { loginStateAtom, userAtom } from '../store/user';
 import { useRouter } from 'next/router';
+import useFetch from './useFetch';
+import { loginStateAtom, userAtom } from '../store/user';
+import { API_URL_AUTH } from '../pages/api/auth';
 
-const useAuth = () => {
+interface LoginResponse {
+  tokens?: { access: string; refresh: string };
+  user: { id: number; realname: string };
+}
+
+function useAuth() {
   const router = useRouter();
+  const client = useFetch();
 
   const [isLogin, setIsLogin] = useRecoilState(loginStateAtom);
   const [user, setUser] = useRecoilState(userAtom);
 
   const setLoginState = useCallback(
-    async ({
-      tokens,
-      user,
-    }: {
-      tokens?: { access: string; refresh: string };
-      user: { id: number; realname: string };
-    }) => {
+    async ({ tokens, user }: LoginResponse) => {
       if (isDevMode && tokens) {
         setCookie('access', tokens.access);
         setCookie('refresh', tokens.access);
@@ -33,16 +35,22 @@ const useAuth = () => {
     [router, setIsLogin, setUser],
   );
 
-  const setLogoutState = () => {
-    deleteCookie('access');
-    deleteCookie('refresh');
-    localStorage.removeItem('AUTH_STATE');
+  // 로그아웃
+  const logout = async () => {
+    try {
+      client.post(API_URL_AUTH.LOGOUT, null);
 
-    setIsLogin(false);
-    setUser({ id: 0, realname: '' });
+      deleteCookie('access');
+      deleteCookie('refresh');
+      localStorage.removeItem('AUTH_STATE');
+
+      location.href = '/';
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  return { isLogin, user, setLoginState, setLogoutState };
-};
+  return { isLogin, user, setLoginState, logout };
+}
 
 export default useAuth;
