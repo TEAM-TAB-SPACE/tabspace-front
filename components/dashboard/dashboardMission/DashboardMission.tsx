626daebc6c99c2894a2ac633dc3c852a93e65c52
moonyerim2 <moonyerim2@gmail.com>
2023-04-25 13:20:55 +0900

    refactor: mission 데이터를 useMission 훅에서 관리하도록 수정

--- components/dashboard/dashboardMission/DashboardMission.tsx
+++ components/dashboard/dashboardMission/DashboardMission.tsx
@@ -1,5 +1,4 @@
-import { useEffect } from 'react';
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
 
 import ProgressWithBackground from 'components/common/ProgressWithBackground';
 import SpinCircle from 'components/common/SpinCircle';
@@ -8,16 +7,11 @@ import MissionList from './MissionList';
 import useFetch from 'hooks/useFetch';
 import useMission from 'hooks/useMission';
 
-import {
-  missionsAtom,
-  MissionSingleData,
-  missionsRefetchKeyAtom,
-} from 'store/dashboard';
+import { missionsRefetchKeyAtom } from 'store/dashboard';
 
 import { API_URL_DASHBOARD } from 'pages/api/dashboard';
 
 function DashboardMission() {
-  const setMission = useSetRecoilState<MissionSingleData[]>(missionsAtom);
   const [refetchKey, setRefetchKey] = useRecoilState(missionsRefetchKeyAtom);
 
   const { isLoading, data } = useFetch({
@@ -26,10 +20,6 @@ function DashboardMission() {
   });
 
   const { missions, percent } = useMission(data);
-
-  useEffect(() => {
-    setMission(missions);
-  }, [missions, setMission]);
 
   if (isLoading)
     return <SpinCircle style={{ width: '100%', height: '250px' }} />;
